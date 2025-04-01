c9c965dcab34d9ed408599b3d98786d5ec736b87
Kawannrl <kawann.rl08@gmail.com>
2025-04-01 17:11:20 -0300

    Finalizamos o login

--- Lista de Tarefas/app.py
+++ Lista de Tarefas/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import database
 
 app = Flask (__name__)
+app.secret_key = "SENHA SECRETA"
 
 @app.route ('/')
 def hello ():
     return render_template ('index.html')
 
-@app.route ('/login')
+@app.route ('/login', methods = ["GET", "POST"])
 def login ():
-    return render_template ('login.html')
+    if request.method == "POST":
+        form = request.form
+        
+        if database.fazer_login (form) == True:
+            session ['usuario'] = form ['email']
+            return redirect (url_for ('lista'))
+        else:
+            return "Ocorreu um erro ao fazer login"
+    else:
+        return render_template ('login.html')
 
 @app.route ('/cadastro', methods = ["GET", "POST"])
 def cadastro ():
@@ -23,6 +33,12 @@ def cadastro ():
             return "Ocorreu um erro ao cadastrar o usuário"
     else:
         return render_template ('cadastro.html')
+    
+@app.route ('/lista')
+def lista ():
+    if 'usuario' not in session:
+        return redirect (url_for ('login'))
+    return render_template ('lista.html')
 
 if __name__ == '__main__':
     app.run (debug = True)
--- Lista de Tarefas/database.py
+++ Lista de Tarefas/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def conectar_banco ():
     conexao = sqlite3.connect ("tarefas.db")
@@ -22,9 +23,30 @@ def criar_usuario (formulario):
         print ("LOG: Já existe esse e-mail cadastrado no banco!")
         return False
     
-    cursor.execute ('''insert into usuarios (email, nome, senha) values (?, ?, ?)''', (formulario ['email'], formulario ['nome'], formulario ['senha']))
+    senha_criptografada = generate_password_hash (formulario ['senha'])
+    cursor.execute ('''insert into usuarios (email, nome, senha) values (?, ?, ?)''', (formulario ['email'], formulario ['nome'], senha_criptografada))
     conexao.commit ()
     return True
+
+def fazer_login (formulario):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''select count (email) from usuarios where email = ?''', (formulario ['email'],))
+    conexao.commit ()
+
+    quantidade_de_email = cursor.fetchone ()
+    print (quantidade_de_email)
+    if (quantidade_de_email [0] < 0):
+        print ("LOG: Já existe esse e-mail cadastrado no banco!")
+        return False
+    
+    else:
+        cursor = conexao.cursor ()
+        cursor.execute ('''select senha from usuarios where email = ?''', (formulario ['email'],))
+        conexao.commit ()
+        senha_criptografada = cursor.fetchone ()
+        resultado_verificacao = check_password_hash (senha_criptografada [0], formulario ['senha'])
+        return resultado_verificacao
 
 if __name__ == "__main__":
     criar_tabelas ()
