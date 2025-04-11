a7d42e7e50d5e2ec1b39891b747165e881530191
Kawannrl <kawann.rl08@gmail.com>
2025-04-11 17:06:44 -0300

    Começamos um novo projeto

--- MelodiaLab/app.py
+++ MelodiaLab/app.py
@@ -1,46 +1,60 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
-app = Flask(__name__)
+from flask import Flask, render_template, request,session, url_for, redirect, flash
+import database
+
+app = Flask (__name__)
 app.secret_key = "chave_muito_segura"
 
 # Cria um dicionário e usuários e senha, SERÁ MIGRADO PARA O BANCO DE DADOS
-usuarios = {
-    'usuario' : 'senha',
-    'admin' : 'admin'
-}
 
-@app.route('/') #rota para a página inicial
-def index():
-    return render_template('index.html')
+@app.route ('/') #rota para a página inicial
+def index ():
+    return render_template ('index.html')
 
-@app.route('/login') #rota para a página de login
-def login():
-    return render_template('login.html')
+@app.route ('/home')
+def home ():
+    return render_template ('home.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route ('/cadastro') #rota para a página de login
+def cadastro ():
+    return render_template ('cadastro.html')
 
-
-# VERFIFICAR O LOGIN
-@app.route('/verificar-login', methods=['POST'])
-def verificar_login():
-# Pegando o que o usuário digitou no campo de entrada de user e senha
-    username = request.form['username']
-    password = request.form['password']
-
-    # Verifica se o usuario digitado está na lista e se 
-    # a senha está certa
-    if username in usuarios and usuarios[username] == password:
-        return redirect(url_for('home'))
+@app.route ('/cadastrar', methods = ["POST"])
+def cadastrar ():
+    if request.method == "POST":
+        form = request.form
+        
+        email = form ["email"]
+        nome = form ["nome"]
+        senha = form ["senha"]
+        
+        if database.criar_usuario (email, nome, senha) == True:
+            return render_template ('login.html')
+        else:
+            return ("Ocorreu um erro ao cadastrar o usuário!")
     else:
-        # Flash envia mensagem para o front-end
-        flash('Usuário ou senha incorretos', 'danger')
-        return redirect(url_for('login'))
-
-@app.route('/cadastro') #rota para a página de login
-def cadastro():
-    return render_template('cadastro.html')
+        return render_template ('cadastro.html')
+    
+@app.route ('/login', methods = ["GET", "POST"]) #rota para a página de login
+def login ():
+    if request.method == "POST":
+        form = request.form
+        
+        email = form ["email"]
+        senha = form ["senha"]
+        
+        if database.fazer_login (email, senha) == True:
+            session ['nome'] = (email,)
+            return redirect (url_for ('home'))
+        else:
+            return ("Ocorreu um erro ao  fazer login!")
+    else:
+        return render_template ('login.html')
+    
+@app ('/home', methods = ["GET", "POST"])
+def nova_musica ():
+    if request.method == "POST":
+        pass
 
 # parte principal do
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run (debug = True)
--- MelodiaLab/database.py
+++ MelodiaLab/database.py
@@ -1,5 +1,56 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def conectar_banco ():
+    conexao = sqlite3.connect ("musicas.db")
+    return conexao
+
+def criar_tabelas ():
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''create table if not exists usuarios (email text primary key, nome text, senha text)''')
+    cursor.execute ('''create table if not exists musicas (id integer primary key, id_usuario text, musica_nome text, artista text, status integer, imagem text, letra text)''')
+    conexao.commit ()
+    
+def criar_usuario (email, nome , senha):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''select count (email) from usuarios where email = ?''', (email,))
+    conexao.commit ()
+    quantidade_de_email = cursor.fetchone ()
+    
+    if (quantidade_de_email [0] > 0):
+        print ("LOG: Já existe esse e-mail cadastrado no banco!")
+        return False
+    
+    senha_criptografada = generate_password_hash (senha)
+    cursor.execute ('''insert into usuarios (email, nome, senha) values (?, ?, ?)''', (email, nome, senha_criptografada))
+    conexao.commit ()
+    return True
+
+def fazer_login (email, senha):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''select count (email) from usuarios where email = ?''', (email,))
+    quantidade_de_email = cursor.fetchone ()
+    
+    if (quantidade_de_email [0] < 0):
+        print ("LOG: Já existe esse email cadastrado no banco de dados!")
+        return False
+    else:
+        cursor.execute ('''select senha from usuarios where email = ?''', (email,))
+        senha_criptografada = cursor.fetchone ()
+        verificar_senha = check_password_hash (senha_criptografada [0], senha)
+        return verificar_senha
+    
+def apagar_usuario (email):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''delete from usuarios where email = ?''', (email,))
+    cursor.execute ('''delete from musicas where email = ?''', (email,))
+    conexao.commit ()
+        
 
 # PARTE PRINCIPAL DO PROGRAMA
 if __name__ == '__main__':
-    print("Hello, world!")
+    criar_tabelas ()
