6e31e301986dceaf46217e27fa620c617650b6dc
Kawannrl <kawann.rl08@gmail.com>
2025-04-17 17:10:58 -0300

    continuamos o projeto

--- KawTV/app.py
+++ KawTV/app.py
@@ -1,16 +1,44 @@
 from flask import Flask, render_template, request,session, url_for, redirect, flash
 import database
 
-app = Flask (__name__)
-app.secret_key = "chave_muito_segura"
+app = Flask(__name__)
+app.secret_key = 'chave-secreta-do-ourico'
 
 @app.route ('/')
 def index ():
-    return render_template ('index.html')
+    return redirect (url_for ('login'))
 
-@app.route ('/cadastro')
-def cadastro ():
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route ('/confirmar-cadastro', methods = ['GET', 'POST'])
+def cadastrar ():
+    if request.method == "POST":
+        form = request.form
+        
+        if database.criar_usuario (form) == True:
+            return render_template ('login.html')
+        else:
+            return ("Ocorreu um erro ao cadastrar o usuário!")
+
     return render_template ('cadastro.html')
-
+    
+@app.route ('/login', methods = ["GET", "POST"])
+def login ():
+    if request.method == "POST":
+        form = request.form
+        
+        email = form ["email"]
+        senha = form ["senha"]
+        
+        if database.fazer_login (email, senha) == True:
+            session ['nome'] = (email)
+            return redirect (url_for ('home'))
+        else:
+            return ("Ocorreu um erro ao  fazer login!")
+    else:
+        return render_template ('login.html')
+    
 if __name__ == '__main__':
     app.run (debug = True)
--- KawTV/database.py
+++ KawTV/database.py
@@ -2,11 +2,54 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def conectar_banco ():
-    conexao = sqlite3.connect ("musicas.db")
+    conexao = sqlite3.connect ("Ourico_TV.db")
     return conexao
 
 def criar_tabelas ():
-    pass
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''create table if not exists usuarios (email text primary key, nome text, senha text)''')
+    cursor.execute ('''create table if not exists filmes (id integer primary key, id_usuario text, titulo text, estilo text, ano text, descricao text, imagem text)''')
+    conexao.commit ()
+    conexao.close ()
+    
+def criar_usuario (formulario):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''select count (email) from usuarios where email = ?''', (formulario['email'],))
+    conexao.commit ()
+    quantidade_de_email = cursor.fetchone ()
+    
+    if (quantidade_de_email [0] > 0):
+        print ("LOG: Já existe esse e-mail cadastrado no banco!")
+        return False
+    
+    senha_criptografada = generate_password_hash (formulario['senha'])
+    cursor.execute ('''insert into usuarios (email, nome, senha) values (?, ?, ?)''', (formulario['email'], formulario['nome'], senha_criptografada))
+    conexao.commit ()
+    return True
+
+def fazer_login (formulario):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''select count (email) from usuarios where email = ?''', (formulario['email'],))
+    quantidade_de_email = cursor.fetchone ()
+    
+    if (quantidade_de_email [0] < 0):
+        print ("LOG: Já existe esse email cadastrado no banco de dados!")
+        return False
+    else:
+        cursor.execute ('''select senha from usuarios where email = ?''', (formulario['email'],))
+        senha_criptografada = cursor.fetchone ()
+        verificar_senha = check_password_hash (senha_criptografada [0], formulario['senha'])
+        return verificar_senha
+    
+def adicionar_filme (usuario_id, titulo, estilo, ano, descricao, imagem):
+    conexao = conectar_banco ()
+    cursor = conexao.cursor ()
+    cursor.execute ('''insert into filmes (usuario_id = ?, titulo = ?, estilo = ?, ano = ?, descricao = ?, imagem = ?) values (?, ?, ?, ?, ?, ?)''', (usuario_id, titulo, estilo, ano, descricao, imagem))
+    conexao.commit 
+    return True
 
 if __name__ == '__main__':
     criar_tabelas ()
